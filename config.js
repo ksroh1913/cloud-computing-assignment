79712f96abc916bbbf529945258b5b67cc1e8448
KYEONG SOO ROH <nohks1913@gmail.com>
2026-09-13 13:50:46 +0900

    Render API 주소 연결

--- config.js
+++ config.js
@@ -1,4 +1,3 @@
 window.APP_CONFIG = Object.freeze({
-  // Render 배포 후 생성된 주소로 교체합니다. 끝에는 /를 붙이지 않습니다.
-  API_BASE_URL: "http://127.0.0.1:8000",
+  API_BASE_URL: "https://kyeongsoo-profile-api.onrender.com",
 });
